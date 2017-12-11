6a705d03836edb41186d8fa735bf4767f2e07a96
Chris Reynolds <chris@jazzsequence.com>
2017-12-11 15:23:16 -0700

    support revisions

--- inc/cpt/namespace.php
+++ inc/cpt/namespace.php
@@ -10,7 +10,7 @@ namespace AddressBook\CPT;
 function register_address() {
 	register_extended_post_type( 'ab_address', [
 			'menu_icon' => 'dashicons-id',
-			'supports'  => ['title'],
+			'supports'  => [ 'title', 'revisions' ],
 		], [
 			'singular' => esc_html__( 'Address', 'address-book' ),
 			'plural'   => esc_html__( 'Addresses', 'address-book' ),
