58a7c95a608766bc14637c1e40ebe42dec7dfdde
Chris Reynolds <chris@jazzsequence.com>
2017-12-12 17:29:34 -0700

    move the show on logic to the show_on_cb function

--- inc/cmb/namespace.php
+++ inc/cmb/namespace.php
@@ -55,19 +55,19 @@ function address_meta() {
 function past_addresses() {
 	$prefix = '_ab_';
 
-	if ( ! empty( $_GET['post'] ) && wp_get_post_revisions( wp_unslash( absint( $_GET['post'] ) ) ) ) {
-		$cmb = new_cmb2_box( [
-			'id'           => $prefix . 'old_addresses',
-			'title'        => __( 'Former Addresses', 'address-book' ),
-			'object_types' => [ 'ab_address' ],
-			'show_names'   => false,
-		] );
+	$cmb = new_cmb2_box( [
+		'id'           => $prefix . 'old_addresses',
+		'title'        => __( 'Former Addresses', 'address-book' ),
+		'object_types' => [ 'ab_address' ],
+		'show_names'   => false,
+		'show_on_cb'   => __NAMESPACE__ . '\\show_past_addresses',
+	] );
 
-		$cmb->add_field( [
-			'name'       => __( 'Old addresses', 'address-book' ),
-			'id'         => $prefix . 'old_addresses',
-			'type'       => 'address_history',
-		] );
+	$cmb->add_field( [
+		'name'       => __( 'Old addresses', 'address-book' ),
+		'id'         => $prefix . 'old_addresses',
+		'type'       => 'address_history',
+	] );
 }
 
 /**
