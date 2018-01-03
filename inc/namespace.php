345e39fd7df2c99ecfcc8fdccdc9384c01902647
Chris Reynolds <chris@jazzsequence.com>
2018-01-03 10:12:42 -0700

    adds new function to handle and build the WP_Query

--- inc/namespace.php
+++ inc/namespace.php
@@ -67,3 +67,24 @@ function get_post_id( $post_id = 0 ) {
 
 	return $post_id;
 }
+
+/**
+ * Handle the query to pull addresses and sort by relationship and family.
+ *
+ * @since  0.2.2
+ * @param  int  $numposts The number of posts to query. -1 pulls all posts.
+ * @param  bool $inactive Whether to include inactive addresses.
+ * @return array          An array of addresses.
+ */
+function address_query( $numposts = -1, $inactive = false ) {
+	$address_query = new \WP_Query([
+		'post_type'      => 'ab_address',
+		'nopaging'       => true,
+		'posts_per_page' => $numposts,
+		'no_found_rows'  => true,
+	]);
+
+	$addresses = (array) $address_query;
+
+	return $addresses['posts'];
+}
