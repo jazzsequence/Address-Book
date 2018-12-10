9bb25a62f1f6e3d1a6b7fb6d847a6b25fe8442ca
Chris Reynolds <chris@jazzsequence.com>
2018-12-10 09:25:09 -0700

    load extended cpts if it wasn't already loaded

--- plugin.php
+++ plugin.php
@@ -6,14 +6,17 @@
  * Author: Chris Reynolds
  * Author URI: https://chrisreynolds.io
  * License: GPLv3
- * Version: 0.2.2
+ * Version: 0.2.3
  *
  * @package AddressBook
  */
 
 namespace AddressBook;
 
-require_once __DIR__ . '/vendor/johnbillion/extended-cpts/extended-cpts.php';
+// Load extended cpts if it hasn't been loaded yet.
+if ( ! function_exists( 'register_extended_post_type' ) ) {
+	require_once __DIR__ . '/vendor/johnbillion/extended-cpts/extended-cpts.php';
+}
 
 require_once __DIR__ . '/inc/namespace.php';
 require_once __DIR__ . '/inc/cpt/namespace.php';
